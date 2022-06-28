c4349146643ddc8fc90fd3506bfeb20c85dcee20
Muhammad Al Juburi <maljuburi89@gmail.com>
2022-06-28 10:34:53 -0700

    stop health

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 'use strict'
 const stun = require('stun')
-const { httpHealth } = require('./http-health')
+// const { httpHealth } = require('./http-health')
 const PORT = 3478
 const server = stun.createServer({ type: 'udp4' })
 const { STUN_BINDING_RESPONSE, STUN_EVENT_BINDING_REQUEST } = stun.constants
 const userAgent = `node/${process.version} stun/v1.0.0`
 
 // a health-check for http
-httpHealth()
+// httpHealth()
 
 server.on(STUN_EVENT_BINDING_REQUEST, (req, rinfo) => {
   const message = stun.createMessage(STUN_BINDING_RESPONSE, req.transactionId)
